7c96feed2b1dc8209f359aeb41efb94cfce11385
Vladi51ove <upworker@inbox.ru>
2024-09-24 10:48:31 +0300

    .

--- back/apps/catalog/queries.py
+++ back/apps/catalog/queries.py
@@ -2,26 +2,32 @@ from apps.core.postgresql_connection import cur
 
 def get_filtered_products_from_db(filters: dict):
 
-    if filters['category'] == 'all' and filters['brands'] == 'all':
+    if filters['category'][0] == 'all' and filters['brands'][0] == 'all':
         cur.execute(
-            f'select * from products where price between {filters['price_min']} and {filters['price_max']}'
+            f'select * from products where price between {filters['price-min'][0]} and {filters['price-max'][0]}'
         )
         products = cur.fetchall()
-        print('1', products)
         return products
 
-    elif filters['category'] != 'all' and filters['brands'] == 'all':
+    elif filters['category'][0] != 'all' and filters['brands'][0] == 'all':
         cur.execute(
-            f"select * from products where price between {filters['price_min']} and {filters['price_max']}and '{filters['category']}'"
+            f"select * from products where price between {filters['price-min'][0]} and {filters['price-max'][0]} and category = '{filters['category'][0]}'"
         )
         products = cur.fetchall()
-        print('2', products)
         return products
 
-    elif filters['category'] == 'all' and filters['brands'] != 'all':
+    elif filters['category'][0] == 'all' and filters['brands'][0] != 'all':
         cur.execute(
-            f"select * from products where price between {filters['price_min']} and {filters['price_max']}and '{filters['brands']}'"
+            f"select * from products where price between {int(filters['price-min'][0])} and {int(filters['price-max'][0])} and brand = '{filters['brands'][0]}'"
         )
         products = cur.fetchall()
-        print('3', products)
         return products
+
+
+def get_product_by_search(product_name: str):
+    cur.execute(
+        f"select * from products where productname = '{product_name}'"
+    )
+    product = cur.fetchone()
+    print('PRODUCT NAME', product)
+    return product
--- back/apps/catalog/views.py
+++ back/apps/catalog/views.py
@@ -5,36 +5,7 @@ from django.views import View
 import asyncio
 
 from apps.core.postgresql_connection import cur
-from .queries import get_filtered_products_from_db
-
-# def catalog_page(request):
-#     if request.method == 'GET':
-#         cur.execute('select * from products')
-#         all_products = cur.fetchall()
-
-#         paginator = Paginator(all_products, 10)
-#         page_number = request.GET.get('page')
-#         products = paginator.get_page(page_number)
-#         # TODO: сформировать корректную url строку для get запроса с учетом фильтров
-
-#         filters = dict(request.GET) # {'price-min': ['105'], 'price-max': ['515'], 'category': ['all'], 'brands': ['all'], 'quantity': ['9']}
-#         if filters:
-
-#             category_filter:int | str = int(filters['category'][0]) if filters['category'][0].isdigit() else 'all'
-#             print('CATEGORY FILTER',category_filter, type(category_filter))
-#             if category_filter.isdigit():
-#                 cur.execute(
-#                     f"select * from products where price between {int(filters['price-min'][0])} and {int(filters['price-max'][0])} and category = '{category_filter}' and brand = '{filters['brands'][0]}'")
-#                 filtered_products = cur.fetchall()
-#                 # print(filtered_products)
-#                 return render(request, 'catalog.html', {'products': products})
-
-#             cur.execute(
-#                 f"select * from products where price between {int(filters['price-min'][0])} and {int(filters['price-max'][0])} and brand = '{filters['brands'][0]}' order by price")
-#             filtered_products = cur.fetchall()
-#             # print(filtered_products)
-#             return render(request, 'catalog.html', {'products': products })
-#     return render(request, 'catalog.html', {'products': products})
+from .queries import get_filtered_products_from_db, get_product_by_search
 
 
 def product_page(request, product_id):
@@ -48,36 +19,32 @@ def add_to_cart(request):
 class CatalogPageView(View):
     template_name = 'catalog.html'
 
-    def get_filters(self):
-        q = self.request.GET.copy()
-        q.pop(self.page_kwarg, None)
-        print("URLENCODE", q.urlencode())
-        return q.urlencode()
-
-    def path_without_page(self):
-        print("PATH WITHOUT PAGE", f'{self.request.path}?{self.get_filters()}')
-        return f'{self.request.path}?{self.get_filters()}'
-
-    async def get(self, request):
+    def get(self, request):
         cur.execute('select * from products')
         all_products = cur.fetchall()
 
         page_number = request.GET.get('page')
         qty_per_page = request.GET.get('quantity')
-        paginator = Paginator(all_products, 10)
-        products = paginator.get_page(page_number)
-        context = {
-            'msg': 'thisis message'
-        }
-        filters = dict(request.GET) # {'price-min': ['105'], 'price-max': ['515'], 'category': ['all'], 'brands': ['all'], 'quantity': ['9']}
-        if not filters:
-            print('NOT FILTERS', request.GET)
-            context['products'] = products
+        paginator = Paginator(all_products, 9)
+        print('QTYPER', type(qty_per_page), qty_per_page)
+        context = {}
+
+        filters: dict = dict(request.GET) # {'price-min': ['105'], 'price-max': ['515'], 'category': ['all'], 'brands': ['all'], 'quantity': ['9']}
+
+        if filters and len(filters) > 1:
+            filtered_products = get_filtered_products_from_db(filters)
+            qty_per_page = request.GET.get('quantity')
+            paginator = Paginator(filtered_products, qty_per_page)
+            context['products'] = paginator.get_page(page_number)
             return render(request, self.template_name, context)
 
-        else:
-            # TODO: не доходит запрос к БД
-            filtered_products = get_filtered_products_from_db(filters)
-            print('FILTERED', filtered_products)
-            context['products'] = filtered_products
+        if not filters or len(filters) == 1:
+            if filters.get('q'):
+                print('Q IN FILTERS')
+                product = get_product_by_search(filters['q'][0])
+                context['product'] = product
+                return render(request, self.template_name, context)
+            print('NOTHING')
+            products = paginator.get_page(page_number)
+            context['products'] = products
             return render(request, self.template_name, context)
